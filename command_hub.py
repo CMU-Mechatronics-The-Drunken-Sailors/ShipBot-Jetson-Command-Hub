36c98e44d85b05bc69ae62f0bb2abcfa7f273502
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2022-03-23 13:24:19 -0400

    Create command_hub.py

---
+++ command_hub.py
@@ -0,0 +1,5 @@
+# command hub for Jetson Xavier NX
+# communicates with Uno, Mega, and SKR boards
+# for Mega, sends command of 4 values indicating velocity for each DC motor (deg/sec)
+# for Uno, sends command of 3 values indicating direction of linear actuators (forward, back, stop)
+# for SKR, sends command of N values indicating stepper motor velocity
