99d988c5820aed14ea125a8fb0f4b76fdefea47d
Brandon Joel Gonzalez <bgonzale@andrew.cmu.edu>
2022-03-30 15:02:37 -0400

    Added echo, need to debug

--- command_hub.py
+++ command_hub.py
@@ -8,20 +8,22 @@ import serial
 import sys
 
 # open ports - how to automate port assignment? see: https://stackoverflow.com/questions/24214643/python-to-automatically-select-serial-ports-for-arduino
-# mega_port = serial.Serial('/dev/ttyACM0', '115200')
-# uno_port = serial.Serial('/dev/ttyACM1', '115200')
+mega_port = serial.Serial('/dev/ttyACM0', '115200')
+uno_port = serial.Serial('/dev/ttyACM1', '115200')
 # SKR_port = serial.Serial('/dev/ttyACM2', '115200')
 
 while (1):
   # grab input for DC motors and send to Mega
   DC_motor_commands = input("DC Motor Commands:\n")
-  print("Sent: " + DC_motor_commands)
-  # mega_port.write(DC_motor_commands)
+  mega_port.write(DC_motor_commands.encode())
+  mega_echo = mega_port.readline()
+  print(mega_echo)
   
   # grab input for linear actuators and send to Uno
   linear_actuator_commands = input("Linear Actuator Commands:\n")
-  print("Sent: " + linear_actuator_commands)
-#   uno_port.write(linear_motor_commands)
+  uno_port.write(linear_actuator_commands.encode())
+  uno_echo = uno_port.readline()
+  print(uno_echo)  
   
   # grab input for stepper motors and send to SKR
   # stepper_motor_commands = input("Stepper Motor Commands")
