76ae70f9aee3ffd79b4ab0d2f933d43a480fe56b
Brandon Joel Gonzalez <bgonzale@andrew.cmu.edu>
2022-04-05 22:25:52 -0400

    Debugging

--- command_hub.py
+++ command_hub.py
@@ -10,22 +10,23 @@ import time
 
 # open ports - how to automate port assignment? see: https://stackoverflow.com/questions/24214643/python-to-automatically-select-serial-ports-for-arduino
 mega_port = serial.Serial('/dev/ttyACM0', '115200')
-# uno_port = serial.Serial('/dev/ttyACM1', '115200')
-# SKR_port = serial.Serial('/dev/ttyACM2', '115200')
+# uno_port = serial.Serial('/dev/ttyACM2', '115200')
+# SKR_port = serial.Serial('/dev/ttyACM3', '115200')
 
-serial_wait = 0.5
+serial_wait = 1.0
 
 while (1):
   # grab input for DC motors and send to Mega
   DC_motor_commands = input("DC Motor Commands:\n")
   mega_port.write(bytes(DC_motor_commands, 'utf-8'))
+  # print(DC_motor_commands)
   time.sleep(serial_wait)
-  mega_echo = mega_port.readline()
-  print(mega_echo)
+  # mega_echo = mega_port.readline()
+  #print(mega_echo)
   
   # grab input for linear actuators and send to Uno
-  #linear_actuator_commands = input("Linear Actuator Commands:\n")
-  #uno_port.write(linear_actuator_commands.encode())
+  # linear_actuator_commands = input("Linear Actuator Commands:\n")
+  # uno_port.write(linear_actuator_commands.encode())
   #uno_echo = uno_port.readline()
   #print(uno_echo)  
   
