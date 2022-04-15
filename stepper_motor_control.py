da40ab7c3ef47c5d704487dcab7ef331067096d3
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2022-04-14 21:11:00 -0400

    Update stepper_motor_control.py

--- stepper_motor_control.py
+++ stepper_motor_control.py
@@ -1,1 +1,11 @@
 # sends new stepper motor positions to the SKR
+
+import initialization
+
+def get_stepper_commands():
+    stepper_commands = input("Stepper Motor Commands:\n")
+    stepper_commands += '\n'
+    SKR_port.write(stepper_commands.encode())
+    SKR_echo = SKR_port.readline()
+    print(SKR_echo)
+  
