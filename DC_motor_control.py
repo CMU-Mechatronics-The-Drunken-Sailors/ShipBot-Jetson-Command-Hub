14917f967b87c319ab48f7a93edf21e2db1ffc26
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2022-04-14 21:07:55 -0400

    Update DC_motor_control.py

--- DC_motor_control.py
+++ DC_motor_control.py
@@ -1,1 +1,10 @@
 # sends new DC motor speeds to the Mega
+
+import initialization
+
+def get_DC_commands():
+    DC_commands = input("DC Motor Commands:\n")
+    DC_commands += '\n'
+    mega_port.write(DC_commands.encode())
+    mega_echo = mega_port.readline()
+    print(mega_echo)
