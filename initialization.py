1712f0c7725ca9c21460078d4bdf94c0b3358844
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2022-04-14 21:04:31 -0400

    Update initialization.py

--- initialization.py
+++ initialization.py
@@ -1,1 +1,30 @@
 # establishes USB port connections between Jetson and peripheral devices (Mega, Uno, SKR, RealSense)
+
+import serial
+import serial.tools.list_ports
+import sys
+
+BAUD_RATE = 115200
+mega_port = ''
+uno_port = ''
+SKR_port = ''
+
+# open ports
+def initialize_ports():
+    global mega_port, uno_port, SKR_port
+    ports = list(serial.tools.list_ports.comports())
+    
+    for p in ports:
+        print(p)
+
+        if "Mega" in p.description:
+            mega_port = serial.Serial(p.device, BAUD_RATE)
+            print("Mega found!")
+
+        elif "Uno" in p.description:
+            uno_port = serial.Serial(p.device, BAUD_RATE)
+            print("Uno found!")
+
+        elif "Mode" in p.description:
+            SKR_port = serial.Serial(p.device, BAUD_RATE)
+            print("SKR found!")
