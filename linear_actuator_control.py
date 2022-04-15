fa1ec96be2925930d289118106e854dac2d7acde
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2022-04-14 21:10:24 -0400

    Update linear_actuator_control.py

--- linear_actuator_control.py
+++ linear_actuator_control.py
@@ -1,1 +1,10 @@
 # sends new linear actuator commands to the Uno
+
+import initialization
+
+def set_LA_commands():
+    LA_commands = input("Linear Actuator Commands:\n")
+    LA_commands += '\n'
+    uno_port.write(LA_commands.encode())
+    uno_echo = uno_port.readline()
+    print(uno_echo)  
