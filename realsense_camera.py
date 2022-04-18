72eb1de6ef984eaeac6b3ddc9a0f5231647b1118
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2022-04-18 02:41:13 -0400

    Update realsense_camera.py

--- realsense_camera.py
+++ realsense_camera.py
@@ -1,3 +1,35 @@
-# file for realsense camera functions
-# includes initialization function to begin camera stream
-# includes functions that return RGB and depth data from a frame
+# for realsense camera, captures RGB and depth data
+# see: https://github.com/IntelRealSense/librealsense/blob/master/wrappers/python/examples/opencv_viewer_example.py
+
+import pyrealsense2.pyrealsense2 as rs
+import numpy as np
+import time
+import cv2
+
+# takes in camera pipeline and returns tuple of RGB & depth as numpy arrays
+def get_image(pipeline):
+    # wait for a coherent pair of frames: depth and color
+    frames = pipeline.wait_for_frames()
+    depth_frame = frames.get_depth_frame()
+    color_frame = frames.get_color_frame()
+    while not depth_frame or not color_frame:
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        
+    # convert images to numpy arrays
+    depth_image = np.asanyarray(depth_frame.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
+
+    # apply colormap on depth image (image must be converted to 8-bit per pixel first)
+    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+    depth_colormap_dim = depth_colormap.shape
+    color_colormap_dim = color_image.shape
+
+    # if depth and color resolutions are different, resize color image to match depth image for display
+    if depth_colormap_dim != color_colormap_dim:
+        resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+        return (resized_color_image, depth_colormap)
+    else:
+        return (color_image, depth_colormap)
