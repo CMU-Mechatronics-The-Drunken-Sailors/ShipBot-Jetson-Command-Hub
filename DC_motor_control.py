6f020cbceaaf36ef5c6239b827a6e8d7e2785077
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2022-04-18 17:15:15 -0400

    Update DC_motor_control.py

--- DC_motor_control.py
+++ DC_motor_control.py
@@ -4,18 +4,30 @@ import serial
 import sys
 
 # get encoder ticks from Mega
+# returns array of ints - M1 M2 M3 M4
 def get_encoder_ticks(mega_port):
     encoder_request = "E\n"
+    
+    # read from Mega
     mega_port.write(encoder_request.encode())
+    mega_echo = mega_port.readline()
     
+    # parse encoder ticks
+    encoder_ticks = []
+    for ticks in mega_echo.split()
+        try:
+            encoder_ticks.append(int(ticks))
+        except ValueError():
+            pass
     
-    return ()
+    return encoder_ticks
 
 # send motor speeds to Mega
+# accepts series of ints - M1 M2 M3 M4
 def set_DC_motors(mega_port, M1_speed, M2_speed, M3_speed, M4_speed):
     speeds = [M1_speed, M2_speed, M3_speed, M4_speed]
     
-    # format ints to a string
+    # format speeds to a string
     DC_commands = " "
     DC_commands.joint(speeds)
     DC_commands += '\n'
