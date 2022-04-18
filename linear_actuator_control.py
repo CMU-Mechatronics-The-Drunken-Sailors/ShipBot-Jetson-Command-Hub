4bab2d8468021dc5649f2a3d5fcbf28293269263
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2022-04-18 19:46:48 -0400

    Added functions to extend/retract linear actuators

--- linear_actuator_control.py
+++ linear_actuator_control.py
@@ -1,26 +1,29 @@
-# sends new linear actuator commands to the Uno
+# sets linear actuators to one of four positions, described below
 
 import serial
 import sys
+import initialization as init
 
-# sets linear actuators to one of four positions, described below
 # LA pair retracted (R), LA solo retracted (R) -> -255 -255 -> arm is bent, tip is straight
+def retract_pair_retract_solo():
+    set_linear_actuators("-255 -255")
+
 # LA pair retracted (R), LA solo extended (E) -> -255 255 -> arm and tip are bent
+def retract_pair_extend_solo():
+    set_linear_actuators("-255 255")
+
 # LA pair extended (E), LA solo retracted (R) -> 255 -255 -> arm and tip are straight
+def extend_pair_retract_solo():
+    set_linear_actuators("255 -255")
+
 # LA pair extended (E), LA solo extended (E) -> 255 255 -> arm is straight, tip is bent
-def set_linear_actuators(uno_port, LA_commands):
-    # check for one of the four positions above
-    if LA_commands == 'RR':
-        LA_commands = '-255 -255'
-    elif LA_commands == 'RE':
-        LA_commands = '-255 255'
-    elif LA_commands == 'ER':
-        LA_commands = '255 -255'
-    elif LA_commands == 'EE':
-        LA_commands = '255 255'
-        
+def extend_pair_extend_solo():
+    set_linear_actuators("255 255")
+    
+# sets linear actuators, given string
+def set_linear_actuators(LA_commands):    
     # send to Uno
     LA_commands += '\n'
-    uno_port.write(LA_commands.encode())
-    uno_echo = uno_port.readline()
+    init.uno_port.write(LA_commands.encode())
+    uno_echo = init.uno_port.readline()
     print(uno_echo)  
