364e533ec3dff5928872c72d53b72a00fc17586e
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2022-04-20 13:16:38 -0400

    Update command_hub.py

--- command_hub.py
+++ command_hub.py
@@ -4,11 +4,13 @@
 # for Uno, sends command of 2 values indicating direction of linear actuators (pair and solo LAs)
 # for SKR, sends command of 3 values indicating stepper motor positions (X, Y, Z)
 
-import initialization as init
-import DC_motor_control as Mega
-import linear_actuator_control as Uno
-import stepper_motor_control as SKR
-import mission_parser as parser
+import time
+import cv2
+from . import initialization as init
+from . import DC_motor_control as Mega
+from . import linear_actuator_control as Uno
+from . import stepper_motor_control as SKR
+from . import realsense_camera as camera
 
 # user input for testing/debugging
 def manual_control():
@@ -34,19 +36,37 @@ def manual_control():
                 Uno.extend_pair_extend_solo()
   
         # send stepper motor commands to SKR
-        # stepper_commands = input("Stepper Motor Commands:\n") # user input for testing/debugging
-        # if stepper_commands == '':
-        #     return
-        # else:
-        #     SKR.set_stepper_motors(SKR_port, stepper_commands)
+        stepper_commands = input("Stepper Motor Commands:\n") # user input for testing/debugging
+        if stepper_commands != '':
+            SKR.set_stepper_motors(stepper_commands)
+
+def test_camera():
+    print("Reading camera...")
+    while(1):
+        (RGB, depth) = camera.get_image()
+        cv2.imshow('RGB', RGB)
+        cv2.waitKey(0)
+        cv2.imshow('depth', depth)
+        cv2.waitKey(0)
 
 if __name__ == '__main__':
     # open USB ports
     init.initialize_ports()
     init.initialize_camera()
+    time.sleep(1)
 
+    # grab user input and send to Mega/Uno/SKR
     # manual_control()
+    
+    test_camera()
 
     # test DC functions
     # Mega.set_DC_motors(0, 0, 0, 0)
-    # encoder_ticks = Mega.get_encoder_ticks()
+    # while True:
+    #     t = time.time()
+    #     encoder_ticks = Mega.get_encoder_ticks()
+    #     time_to_wait = 0.05 - (time.time() - t)
+    #     if time_to_wait < 0:
+    #         print("WHOA THIS V BAD")
+    #     else:
+    #         time.sleep(time_to_wait)
