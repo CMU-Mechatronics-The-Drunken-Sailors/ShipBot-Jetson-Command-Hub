3248b6c337736c6070dee5cb1044662aeb4adb4f
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2022-04-20 13:17:02 -0400

    Update stepper_motor_control.py

--- stepper_motor_control.py
+++ stepper_motor_control.py
@@ -2,11 +2,26 @@
 
 import serial
 import sys
+import initialization as init
 
+# sets X/Y positions for gantry and Z position for end-effector
+# arguments are optional
+def build_command(x_pos = None, y_pos = None, z_pos = None):
+    command = "G1"
+    
+    if x_pos != None:
+        command += " X" + str(x_pos)
+    elif y_pos != None:
+        command += " Y" + str(y_pos)
+    elif z_pos != None:
+        command += " Z" + str(z_pos)
+        
+    set_stepper_motor(command)
+    
 # send to SKR
-def set_stepper_motors(SKR_port, stepper_commands):
+def set_stepper_motors(stepper_commands):
     stepper_commands += '\n'
-    SKR_port.write(stepper_commands.encode())
-    SKR_echo = SKR_port.readline()
+    init.write(stepper_commands.encode())
+    SKR_echo = init.readline()
     print(SKR_echo)
   
